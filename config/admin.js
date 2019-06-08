da7b1cc60d08dcdbb89eeeafb38e1a3f9f70fd61
deepaksingh <deepak17.singh@paytm.com>
2019-06-08 18:08:42 +0530

    added secured keys

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
-const serverUrl = process.env.SERVERURL || 'localhost:3001';
+const serverUrl = process.env.SERVERURL || "localhost:3001";
 // config used by dashboard client side only
 module.exports = {
-	// dashboard UI language
-	language: 'en',
-	apiBaseUrl: `https://${serverUrl}/api/v1`,
-	apiWebSocketUrl: `wss://${serverUrl}`,
-	developerMode: true
+  // dashboard UI language
+  language: "en",
+  apiBaseUrl: `https://${serverUrl}/api/v1`,
+  apiWebSocketUrl: `wss://${serverUrl}`
+  // developerMode: true
 };
--- config/server.js
+++ config/server.js
@@ -10,64 +10,64 @@
 // const dbUrl =
 // 	process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`;
 const dbUrl =
-	process.env.DB_URL ||
-	// 'mongodb://shashank:g9971441252@ds139341.mlab.com:39341/cart';
-	// 'mongodb://shashank:g9971441252@ds155396.mlab.com:55396/tobaniware';
-	'mongodb://shashank:g9971441252@ds139341.mlab.com:39341/cart';
+  process.env.DB_URL ||
+  // 'mongodb://shashank:g9971441252@ds139341.mlab.com:39341/cart';
+  // 'mongodb://shashank:g9971441252@ds155396.mlab.com:55396/tobaniware';
+  "mongodb://shashank:g9971441252@ds139341.mlab.com:39341/cart";
 
-const serverUrl = process.env.SERVERURL || 'localhost:3001';
-const clientUrl = process.env.CLIENTURL || 'localhost:3000';
+const serverUrl = process.env.SERVERURL || "localhost:3001";
+const clientUrl = process.env.CLIENTURL || "localhost:3000";
 
 module.exports = {
-	apiListenPort: process.env.PORT || 3001,
-	storeListenPort: process.env.PORT || 3000,
-	// used by Store (server side)
-	apiBaseUrl: `https://${serverUrl}/api/v1`,
+  apiListenPort: process.env.PORT || 3001,
+  storeListenPort: process.env.PORT || 3000,
+  // used by Store (server side)
+  apiBaseUrl: `https://${serverUrl}/api/v1`,
 
-	// used by Store (server and client side)
-	ajaxBaseUrl: `https://${serverUrl}/ajax`,
+  // used by Store (server and client side)
+  ajaxBaseUrl: `https://${serverUrl}/ajax`,
 
-	// Access-Control-Allow-Origin
-	storeBaseUrl: `https://${clientUrl}`,
+  // Access-Control-Allow-Origin
+  storeBaseUrl: `https://${clientUrl}`,
 
-	// used by API
-	adminLoginUrl: '/admin/login',
-	// used by API
-	mongodbServerUrl: dbUrl,
+  // used by API
+  adminLoginUrl: "/admin/login",
+  // used by API
+  mongodbServerUrl: dbUrl,
 
-	smtpServer: {
-		host: '',
-		port: 0,
-		secure: true,
-		user: '',
-		pass: '',
-		fromName: '',
-		fromAddress: ''
-	},
+  smtpServer: {
+    host: "",
+    port: 0,
+    secure: true,
+    user: "",
+    pass: "",
+    fromName: "",
+    fromAddress: ""
+  },
 
-	// key to sign tokens
-	jwtSecretKey: '-',
+  // key to sign tokens
+  jwtSecretKey: "SP69kXFR3znRi7kL8Max2GTB24wOtEQj",
 
-	// key to sign store cookies
-	cookieSecretKey: '-',
+  // key to sign store cookies
+  cookieSecretKey: "8669X9P5yI1DAEthy1chc3M9EncyS7SM",
 
-	// path to uploads
-	categoriesUploadPath: 'public/content/images/categories',
-	productsUploadPath: 'public/content/images/products',
-	filesUploadPath: 'public/content',
-	themeAssetsUploadPath: 'theme/assets/images',
+  // path to uploads
+  categoriesUploadPath: "public/content/images/categories",
+  productsUploadPath: "public/content/images/products",
+  filesUploadPath: "public/content",
+  themeAssetsUploadPath: "theme/assets/images",
 
-	// url to uploads
-	categoriesUploadUrl: '/images/categories',
-	productsUploadUrl: '/images/products',
-	filesUploadUrl: '',
-	themeAssetsUploadUrl: '/assets/images',
+  // url to uploads
+  categoriesUploadUrl: "/images/categories",
+  productsUploadUrl: "/images/products",
+  filesUploadUrl: "",
+  themeAssetsUploadUrl: "/assets/images",
 
-	// store UI language
-	language: 'en',
+  // store UI language
+  language: "en",
 
-	// used by API
-	orderStartNumber: 1000,
+  // used by API
+  orderStartNumber: 1000,
 
-	developerMode: true
+  developerMode: true
 };
